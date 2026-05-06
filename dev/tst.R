89db817357868f600cbe08fe67b3c772f139f602
Mansour.Mac <mabdoli@fullerton.edu>
2026-05-06 11:16:12 -0700

    R-Sandbox w/ StudentSurvey data

--- dev/tst.R
+++ dev/tst.R
@@ -1,4 +1,32 @@
+# Listing Datasets
+installed.packages() |> 
+  rownames() |> 
+  lapply(function(v) {
+      tmp <- data(package=v)$results |> as.data.frame() |> dplyr::select(-c(LibPath))
+    }) |>
+  do.call(what='rbind') -> lds
+  
+  lds$Dim <- NA
+  for(i in 1:nrow(lds)){
+    lds$Dim[i] <- tryCatch(eval(parse(text=paste0(lds$Package[i], '::', lds$Item[i]))) |> 
+      dim() |> paste0(collapse='x'), error=function(e) NA)
+  } 
+  na.omit(lds) -> lds
+  lds |> 
+    dplyr::mutate(
+      dim=(gregexpr('x', Dim) |> sapply(length))+1,
+      nrow = gsub('(^[0-9]+)x[0-9]+.*', '\\1', Dim) |> as.integer(),
+      ncol = gsub('^[0-9]+x([0-9]+).*', '\\1', Dim) |> as.integer()
+    ) |>
+      na.omit() |> arrange(desc(ncol)) |> 
+      filter(ncol<20, ncol>5, nrow>500, nrow<2000) |> write.csv('R-Datasets.csv')
+
+
+
 # Testing Complexity vs Significance
+
+
+
 
 library(coursekata)
 y<- mtcars$mpg
